5ba9ed5742c4163810715a9c27a82e4618ff901c
TimTheGr8 <tjanssen4@gmail.com>
2021-04-12 08:12:23 -0500

    Added Dr. Willow
    Got Dr. Willow added as a prefab and started the AI states for him.

---
+++ Assets/Scripts/Enemies/SpecificClasses/DrWillow.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrWillow : EnemyBase
+{
+    [SerializeField]
+    private int _hitPoints = 30;
+    [SerializeField]
+    private List<GameObject> _projectiles;
+    [SerializeField]
+    private Transform _throwPosition;
+
+
+    private bool _canMakeDecision = false;
+    private Transform _projectileParent;
+    private WalkingController _wc;
+
+    void Start()
+    {
+        _wc = GetComponent<WalkingController>();
+
+        _projectileParent = GameObject.Find("Projectiles").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (AppleGameManager.Instance.CurrentPlayerTransform.position.x < transform.position.x && _wc.FacingDirection == 1 ||
+            AppleGameManager.Instance.CurrentPlayerTransform.position.x > transform.position.x && _wc.FacingDirection == -1)
+            _wc.Flip();
+        if (_canMakeDecision)
+            MakeDecision();
+    }
+
+    private void MakeDecision()
+    {
+        switch (PickRandomNumber(1,3))
+        {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            default:
+                MakeDecision();
+                break;
+        }
+    }
+
+    public override void LaunchProjectile()
+    {
+        GameObject ammo = Instantiate(_projectiles[PickRandomNumber(0, 2)], _throwPosition.position, transform.rotation);
+        ammo.transform.parent = _projectileParent.transform;
+    }
+
+    private int PickRandomNumber(int min, int max)
+    {
+        int rand = Random.Range(min, max);
+        return rand;
+    }
+
+    IEnumerator DecisionCoolDown(float waitTime)
+    {
+        while (waitTime > 0)
+        {
+            waitTime -= 0.25f;
+            yield return new WaitForSeconds(0.25f);
+        }
+    }
+}
